9b31e2f0226a99463179e5f17888a6a78507f1cd
Dylan Huang <dylan.p.huang@gmail.com>
2018-05-15 00:23:49 -0500

    Voting and Chat Names
     - User can log on to page and obtain a unique id (per device)
     - User can vote and undo their votes if they choose to
     - User can input a name and chat will show accordingly
     - Admin can reset all votes and allow for the next session

--- app.py
+++ app.py
@@ -2,33 +2,67 @@ from gevent import monkey
 monkey.patch_all()
 
 import cgi
+import uuid
 import redis
-from flask import Flask, render_template
 from flask_socketio import SocketIO
+from flask import Flask, render_template
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 db = redis.StrictRedis('localhost', 6379, 0)
 
+YES_VOTE_COUNT_KEY = 'yes_vote_count'
+NO_VOTE_COUNT_KEY = 'no_vote_count'
+USERS_VOTED_KEY = 'users_voted'
+
+# Helpers
+def emit_votes(yes_count, no_count):
+    socketio.emit('vote',
+            {YES_VOTE_COUNT_KEY: yes_count,
+                NO_VOTE_COUNT_KEY: no_count
+                },
+            namespace='/app')
+
+# HTTP routes
 @app.route('/')
 def main():
     return render_template('index.html')
 
-@socketio.on('connect', namespace='/test')
-def ws_conn():
-    c = db.incr('counter')
-    print "Incremented: {}".format(c)
-    socketio.emit('counter', {'count': c}, namespace='/test')
+# WebSocket routes
+@socketio.on('connect', namespace='/app')
+def ws_connect():
+    emit_votes(db.get(YES_VOTE_COUNT_KEY), db.get(NO_VOTE_COUNT_KEY))
+    unique_id = str(uuid.uuid4())
+    socketio.emit('uuid', {'uuid': unique_id}, namespace='/app')
 
-@socketio.on('disconnect', namespace='/test')
-def ws_disconn():
-    c = db.decr('counter')
-    print "Decremented: {}".format(c)
-    socketio.emit('counter', {'count': c}, namespace='/test')
+@socketio.on('msg', namespace='/app')
+def ws_chat(msg):
+    socketio.emit('msg', {'msg': cgi.escape(msg['msg'])}, namespace='/app')
 
-@socketio.on('msg', namespace='/test')
-def ws_msg(msg):
-    socketio.emit('msg', {'msg': cgi.escape(msg['msg'])}, namespace='/test')
+@socketio.on('vote', namespace='/app')
+def ws_vote(msg):
+    if not db.hexists(USERS_VOTED_KEY, msg['uuid']):
+        if msg['vote'] == 'Yes':
+            emit_votes(db.incr(YES_VOTE_COUNT_KEY), db.get(NO_VOTE_COUNT_KEY))
+        elif msg['vote'] == 'No':
+            emit_votes(db.get(YES_VOTE_COUNT_KEY), db.incr(NO_VOTE_COUNT_KEY))
+        db.hset(USERS_VOTED_KEY, msg['uuid'], msg['vote'])
+
+@socketio.on('vote_reset', namespace='/app')
+def ws_vote_reset(msg):
+    db.set(YES_VOTE_COUNT_KEY, 0)
+    db.set(NO_VOTE_COUNT_KEY, 0)
+    db.delete(USERS_VOTED_KEY)
+    emit_votes(db.get(YES_VOTE_COUNT_KEY), db.get(NO_VOTE_COUNT_KEY))
+
+@socketio.on('vote_undo', namespace='/app')
+def ws_vote_undo(msg):
+    vote = db.hget(USERS_VOTED_KEY, msg['uuid'])
+    if vote == 'Yes':
+        emit_votes(db.decr(YES_VOTE_COUNT_KEY), db.get(NO_VOTE_COUNT_KEY))
+    elif vote == 'No':
+        emit_votes(db.get(YES_VOTE_COUNT_KEY), db.decr(NO_VOTE_COUNT_KEY))
+    db.hdel(USERS_VOTED_KEY, msg['uuid'])
 
 if __name__=="__main__":
     socketio.run(app, host="0.0.0.0")
