475eb8c3d5839f485a9817c7b7f8acb3b66b52ac
Dylan Huang <dylan.p.huang@gmail.com>
2018-05-15 21:37:23 -0500

    Set current candidate on connect
    If you are connecting to the website for the first time, it now serves
    the current candidates name and displays it accordingly. This way, if
    you reconnect, it will show the current candidate.

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ socketio = SocketIO(app)
 db = redis.StrictRedis('localhost', 6379, 0)
 
 YES_VOTE_COUNT_KEY = 'yes_vote_count'
+NAMESPACE = '/app'
+CURRENT_CANDIDATE_KEY = 'current_candidate'
 NO_VOTE_COUNT_KEY = 'no_vote_count'
 USERS_VOTED_KEY = 'users_voted'
 
@@ -21,7 +23,7 @@ def emit_votes(yes_count, no_count):
             {YES_VOTE_COUNT_KEY: yes_count,
                 NO_VOTE_COUNT_KEY: no_count
                 },
-            namespace='/app')
+            namespace=NAMESPACE)
 
 # HTTP routes
 @app.route('/')
@@ -33,22 +35,23 @@ def admin():
     return render_template('admin.html')
 
 # WebSocket routes
-@socketio.on('connect', namespace='/app')
+@socketio.on('connect', namespace=NAMESPACE)
 def ws_connect():
     emit_votes(db.get(YES_VOTE_COUNT_KEY), db.get(NO_VOTE_COUNT_KEY))
     unique_id = str(uuid.uuid4())
-    socketio.emit('uuid', {'uuid': unique_id}, namespace='/app')
+    socketio.emit('uuid', {'uuid': unique_id}, namespace=NAMESPACE)
+    socketio.emit('candidate', {'candidate': db.get(CURRENT_CANDIDATE_KEY)}, namespace=NAMESPACE)
 
-@socketio.on('msg', namespace='/app')
+@socketio.on('msg', namespace=NAMESPACE)
 def ws_chat(msg):
-    socketio.emit('msg', {'msg': cgi.escape(msg['msg'])}, namespace='/app')
+    socketio.emit('msg', {'msg': cgi.escape(msg['msg'])}, namespace=NAMESPACE)
 
-@socketio.on('candidate', namespace='/app')
+@socketio.on('candidate', namespace=NAMESPACE)
 def ws_candidate(msg):
-    print msg
-    socketio.emit('candidate', {'candidate': cgi.escape(msg['candidate'])}, namespace='/app')
+    db.set(CURRENT_CANDIDATE_KEY, msg['candidate'])
+    socketio.emit('candidate', {'candidate': cgi.escape(msg['candidate'])}, namespace=NAMESPACE)
 
-@socketio.on('vote', namespace='/app')
+@socketio.on('vote', namespace=NAMESPACE)
 def ws_vote(msg):
     if not db.hexists(USERS_VOTED_KEY, msg['uuid']):
         if msg['vote'] == 'Yes':
@@ -57,14 +60,14 @@ def ws_vote(msg):
             emit_votes(db.get(YES_VOTE_COUNT_KEY), db.incr(NO_VOTE_COUNT_KEY))
         db.hset(USERS_VOTED_KEY, msg['uuid'], msg['vote'])
 
-@socketio.on('vote_reset', namespace='/app')
+@socketio.on('vote_reset', namespace=NAMESPACE)
 def ws_vote_reset(msg):
     db.set(YES_VOTE_COUNT_KEY, 0)
     db.set(NO_VOTE_COUNT_KEY, 0)
     db.delete(USERS_VOTED_KEY)
     emit_votes(db.get(YES_VOTE_COUNT_KEY), db.get(NO_VOTE_COUNT_KEY))
 
-@socketio.on('vote_undo', namespace='/app')
+@socketio.on('vote_undo', namespace=NAMESPACE)
 def ws_vote_undo(msg):
     vote = db.hget(USERS_VOTED_KEY, msg['uuid'])
     if vote == 'Yes':
@@ -74,5 +77,5 @@ def ws_vote_undo(msg):
     db.hdel(USERS_VOTED_KEY, msg['uuid'])
 
 if __name__=="__main__":
-    socketio.run(app, host="0.0.0.0")
+    socketio.run(app, host="0.0.0.0", debug=True)
 
